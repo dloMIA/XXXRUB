4111d8e88c0b96dd962bc4495202268ff8a87434
dloMIA <danielolopezh@gmail.com>
2015-06-12 13:55:45 -0400

    x

---
+++ xxx.rb
@@ -0,0 +1,14 @@
+
+puts "Handle it?"
+n=gets.chomp
+
+while (n!="N")
+	puts "love you, ok?"
+	n=gets.chomp
+end
+
+puts "I just want to love you"
+# May substitute i for var names
+
+
+puts "This is bizzzarrrrrooooo"
