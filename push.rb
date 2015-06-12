de8025d1341a64ae20660399faf83478900f27dc
dloMIA <danielolopezh@gmail.com>
2015-06-12 14:44:37 -0400

    Create push.rb

---
+++ push.rb
@@ -0,0 +1,13 @@
+puts "Handle it?"
+n=gets.chomp
+
+while (n!="N")
+	puts "love you, ok?"
+	n=gets.chomp
+end
+
+puts "I just want to love you"
+# May substitute i for var names
+
+
+puts "push thisssss"
